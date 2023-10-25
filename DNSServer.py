e5d819a19a5a781caeb587414fc4fafa3a3e7229
patrickcarney000 <144631636+patrickcarney000@users.noreply.github.com>
2023-10-25 17:48:55 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -96,7 +96,7 @@ dns_records = {
         dns.rdatatype.AAAA: "2001:0db8:85a3:0000:0000:8a2e:0373:7312",
         dns.rdatatype.MX: [(10, "mxa-00256a01.gslb.pphosted.com.")],
         dns.rdatatype.NS: "ns1.nyu.edu.",
-        dns.rdatatype.TXT: decode(encrypted_value),
+        dns.rdatatype.TXT: str(encrypted_value, 'UTF-8'),
     }
    
     # Add more records as needed (see assignment instructions!
