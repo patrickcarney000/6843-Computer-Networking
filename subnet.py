d8e430e8d252b60834440a20bf5ff6b2ece83035
patrickcarney000 <144631636+patrickcarney000@users.noreply.github.com>
2023-10-27 14:56:28 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 33790
+
+#problem 1 178.122.9.53/26
+p1Network = '178.122.9.0'
+p1Broadcast = '178.122.9.63'
+p1First = '178.122.9.1'
+p1Last =  '178.122.9.62'
+
+#problem 2 89.107.227.96/24
+p2Network = '89.107.227.0'
+p2Broadcast = '89.107.227.256'
+p2First = '89.107.227.1'
+p2Last = '89.107.227.255'
+
+#problem 3 0.92.189.138/27
+p3Network = '0.92.189.138'
+p3Broadcast = '0.92.189.139'
+p3First = '0.92.189.138'
+p3Last = '0.92.189.139'
+
+#problem 4
+p4Network = ''
+p4Broadcast = ''
+p4First = ''
+p4Last = ''
+
+#problem 5
+p5Network = ''
+p5Broadcast = ''
+p5First = ''
+p5Last = ''
+
+#problem 6
+p6Network = ''
+p6Broadcast = ''
+p6First = ''
+p6Last =  ''
+
+#problem 7
+p7Network = ''
+p7Broadcast = ''
+p7First = ''
+p7Last = ''
+
+#problem 8
+p8Network = ''
+p8Broadcast = ''
+p8First = ''
+p8Last = ''
+
+#problem 9
+p9Network = ''
+p9Broadcast = ''
+p9First = ''
+p9Last = ''
+
+#problem 10
+p10Network = ''
+p10Broadcast = ''
+p10First = ''
+p10Last = ''
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
