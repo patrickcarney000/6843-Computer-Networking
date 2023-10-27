405005fac2111a182a041b0b0ea4fa72d7f816bb
patrickcarney000 <144631636+patrickcarney000@users.noreply.github.com>
2023-10-27 16:01:14 -0400

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -9,57 +9,57 @@ p1Last =  '178.122.9.62'
 
 #problem 2 89.107.227.96/24
 p2Network = '89.107.227.0'
-p2Broadcast = '89.107.227.256'
+p2Broadcast = '89.107.227.255'
 p2First = '89.107.227.1'
-p2Last = '89.107.227.255'
+p2Last = '89.107.227.254'
 
 #problem 3 0.92.189.138/27
-p3Network = '0.92.189.138'
-p3Broadcast = '0.92.189.139'
-p3First = '0.92.189.138'
-p3Last = '0.92.189.139'
+p3Network = '0.92.189.128'
+p3Broadcast = '0.92.189.159'
+p3First = '0.92.189.129'
+p3Last = '0.92.189.158'
 
-#problem 4
-p4Network = ''
-p4Broadcast = ''
-p4First = ''
-p4Last = ''
+#problem 4 167.78.151.181/25
+p4Network = '167.78.151.128'
+p4Broadcast = '167.78.151.255'
+p4First = '167.78.151.129'
+p4Last = '167.78.151.254'
 
-#problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+#problem 5 78.63.112.223/28
+p5Network = '78.63.112.208'
+p5Broadcast = '78.63.112.223'
+p5First = '78.63.112.16'
+p5Last = '78.63.112.222'
 
-#problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+#problem 6 245.48.74.10/26
+p6Network = '245.48.74.0'
+p6Broadcast = '245.48.74.63'
+p6First = '245.48.74.1'
+p6Last =  '245.48.74.62'
 
-#problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+#problem 7 156.33.36.52/29
+p7Network = '156.33.36.48'
+p7Broadcast = '156.33.36.55'
+p7First = '156.33.36.49'
+p7Last = '156.33.36.54'
 
-#problem 8
-p8Network = ''
-p8Broadcast = ''
-p8First = ''
-p8Last = ''
+#problem 8 67.18.254.94/27
+p8Network = '67.18.254.64'
+p8Broadcast = '67.18.254.95'
+p8First = '67.18.254.65'
+p8Last = '67.18.254.94'
 
-#problem 9
-p9Network = ''
-p9Broadcast = ''
-p9First = ''
-p9Last = ''
+#problem 9 234.3.215.137/24
+p9Network = '234.3.215.0'
+p9Broadcast = '234.3.215.255'
+p9First = '234.3.215.1'
+p9Last = '234.3.215.254'
 
-#problem 10
-p10Network = ''
-p10Broadcast = ''
-p10First = ''
-p10Last = ''
+#problem 10 145.244.177.179/28
+p10Network = '145.244.177.176'
+p10Broadcast = '145.244.177.191'
+p10First = '145.244.177.177'
+p10Last = '145.244.177.190'
 
 
 """
